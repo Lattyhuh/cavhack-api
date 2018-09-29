ec8338b1ec34982388e64233da04a8c052415fa5
Lee <1526889@fcps.edu>
2018-09-29 17:51:36 -0400

    cavhack submission

--- routes/controller.js
+++ routes/controller.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var Profile = require('../models').profile
-//var Profile = require('../mock/profile')
+//var Profile = require('../models').profile
+var Profile = require('../mock/profile')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendStatus(200)
-});
+  res.send('Hello There')
+})
 
- 
+router.get('/profiles', function (req, res) {
+  res.send(Profile.findAll())
+})
+router.get('/profiles/:id', function (req, res) {
+  var profile = Profile.findById(req.params.id)
+  res.send(profile)
+})
+router.delete('/profiles/:id', function(req, res) {
+  var profiles = Profile.remove(req.params.id)
+  res.send(profiles)
+})
+
+router.put('/profiles/:id', function(req, res) {
+  var profile = Profile.update(req.params.id, {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    bio: req.body.bio,
+    interests: req.body.interests,
+    skills: req.body.skills
+  })
+  res.send(profile)
+})
 module.exports = router;
